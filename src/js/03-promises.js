2e5989a88957cbe7856b46f9e5e9d21ddd80eced
Bondar Serhii <bybondarserhii@gmail.com>
2023-02-08 15:21:27 +0200

    done 2

---
+++ src/js/03-promises.js
@@ -0,0 +1,41 @@
+const firstDelayEl = document.querySelector('input[name="delay"]');
+const delayStepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  let delay = Number(firstDelayEl.value);
+  const step = Number(delayStepEl.value);
+  const amount = Number(amountEl.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(
+          window.alert(`✅ Fulfilled promise ${position} in ${delay}ms`)
+        );
+      })
+      .catch(({ position, delay }) => {
+        console.log(
+          window.alert(`❌ Rejected promise ${position} in ${delay}ms`)
+        );
+      });
+    delay += step;
+  }
+}
